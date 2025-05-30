6e935c002688db1253f64f9e71dac5ed6e97ebed
Armin Vejzovic <29armin.vejzovic@gmail.com>
2025-05-30 12:05:31 +0200

    update of admin dashboard

--- app/src/pages/admin/adminDashboard/AdminDashboard.js
+++ app/src/pages/admin/adminDashboard/AdminDashboard.js
@@ -1,12 +1,47 @@
-import React from 'react'
-import Logout from '../../../components/logout/Logout.js'
+import React from 'react';
+import { FaUserCog, FaUsers, FaClipboardList, FaBlog } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+import Logout from '../../../components/logout/Logout';
+import './AdminDashboard.css';
 
 const AdminDashboard = () => {
+  const dashboardItems = [
+    {
+      title: 'User Management',
+      icon: <FaUsers size={40} />,
+      link: '/admin/users'
+    },
+    {
+      title: 'Adoption Applications',
+      icon: <FaClipboardList size={40} />,
+      link: '/admin/applications'
+    },
+    {
+      title: 'Blog CMS',
+      icon: <FaBlog size={40} />,
+      link: '/admin/blog'
+    },
+    {
+      title: 'Profile',
+      icon: <FaUserCog size={40} />,
+      link: '/profile'
+    }
+  ];
+
   return (
-    <div>AdminDashboard
+    <div className="dashboard-container">
+      <h2>Admin Dashboard</h2>
+      <div className="dashboard-grid">
+        {dashboardItems.map((item, idx) => (
+          <Link to={item.link} key={idx} className="dashboard-item">
+            {item.icon}
+            <span>{item.title}</span>
+          </Link>
+        ))}
+      </div>
       <Logout />
     </div>
-  )
-}
+  );
+};
 
-export default AdminDashboard
+export default AdminDashboard;
